2ee64f149e11696798965b55bf5c6f597126155b
Lucky Strike <91523672+luckystrikegloable@users.noreply.github.com>
2021-10-18 11:48:48 +0800

    Update info.js

--- blockchains/smartchain/assets/0x30571BB45a52A1fa1A8EdaD7bece80BCbB824CC3/info.js
+++ blockchains/smartchain/assets/0x30571BB45a52A1fa1A8EdaD7bece80BCbB824CC3/info.js
@@ -4,7 +4,7 @@
   "symbol": "LKS",
   "type": "BEP20",
   "decimals": 18,
-  "description": "Craft ale meets cryptocurrency",
+  "description": "LuckyStrike is an easy to play blockchain prediction  game that aims to provide a seamless experience of the online blockchain prediction games  interlaced with a user-friendly design and Nfts enable to play to earn.",
   "website": "https://luckystrike.club",
   "explorer": "https://bscscan.com/token/0x30571BB45a52A1fa1A8EdaD7bece80BCbB824CC3",
   "social": {
