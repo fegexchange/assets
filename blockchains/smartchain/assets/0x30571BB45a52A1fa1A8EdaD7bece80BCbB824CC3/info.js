7316967f6c006c8f4be6da8f61706e3d031fa45a
Lucky Strike <91523672+luckystrikegloable@users.noreply.github.com>
2021-10-18 11:47:33 +0800

    Add files via upload

---
+++ blockchains/smartchain/assets/0x30571BB45a52A1fa1A8EdaD7bece80BCbB824CC3/info.js
@@ -0,0 +1,14 @@
+{
+  "id": "0x30571BB45a52A1fa1A8EdaD7bece80BCbB824CC3",
+  "name": "LuckyStrike",
+  "symbol": "LKS",
+  "type": "BEP20",
+  "decimals": 18,
+  "description": "Craft ale meets cryptocurrency",
+  "website": "https://luckystrike.club",
+  "explorer": "https://bscscan.com/token/0x30571BB45a52A1fa1A8EdaD7bece80BCbB824CC3",
+  "social": {
+    "twitter": "https://twitter.com/luckystofficial",
+    "telegram": "https://t.me/luckyStrikeglobal"
+  }
+}
